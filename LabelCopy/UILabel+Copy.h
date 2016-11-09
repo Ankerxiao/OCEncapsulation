93ecc2b585a7d322097dc5514f85abcefcc66460
Beeda <beeda@BeedadeMacBook-Pro.local>
2016-11-09 16:10:57 +0800

    长按label的复制功能

---
+++ LabelCopy/UILabel+Copy.h
@@ -0,0 +1,15 @@
+//
+//  UILabel+Copy.h
+//  BeedaSoftware
+//
+//  Created by Beeda on 16/11/9.
+//  Copyright © 2016年 com.Beeda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (Copy)
+
+@property (nonatomic,assign) BOOL isCopy;
+
+@end
